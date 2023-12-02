19f8c587348fb75489b9ceafdcea1173ffb4baa3
Hudson da Silva Carlos <hudsonscarlos@outlook.com>
2023-12-02 19:55:06 -0300

    feat: subindo atividade 12

---
+++ puc-desenvolvimento-aplicacoes-moveis/aula12/lib/main.dart
@@ -0,0 +1,115 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  int _cep = 32341210;
+
+  void _recuperaCep() async{
+      var uri = Uri.https('viacep.com.br', 'ws/$_cep/json/');
+      http.Response response = await http.get(uri);
+      Map<String, dynamic> retorno = json.decode(response.body);
+      print(response.body);
+  }
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+
+      _recuperaCep();
+      _cep = _cep + 10;
+    });
+  }
+
+  late TextEditingController _controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextField(
+              decoration: const InputDecoration(
+                labelText: "Digíte o CEP ex: 32341210: ",
+              ),
+              controller: _controller,
+              onSubmitted: (String value) async {
+                await showDialog<void>(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: const Text('Thanks!'),
+                      content: Text(
+                          'You typed "$value", which has length ${value.characters.length}.'),
+                      actions: <Widget>[
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: const Text('OK'),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+            ),
+            const Text(
+              "You have pushed the button this many times: ",
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+
+
